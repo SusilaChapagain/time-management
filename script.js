0b42b4f4f14e5a1758c503d748d44d26b0c8f4e2
susila <susilachapagain056@gmail.com>
2022-02-19 00:04:38 +1100

    image updated

--- script.js
+++ script.js
@@ -7,46 +7,40 @@
 // delete the task and re-calculate the hours
 // take task from list to bad list and vs.
 
-
-const taskList = []
-const badList = []
-const hrPerWeak = 168
+const taskList = [];
+const badList = [];
+const hrPerWeak = 168;
 let total;
 
-        
-    
-    
-const handleOnSubmit =(e)=>{
-    
-    const frmData = new FormData(e);
+const handleOnSubmit = (e) => {
+  const frmData = new FormData(e);
 
-    const task = frmData.get("task");
-    const hr = +frmData.get("hr");
+  const task = frmData.get("task");
+  const hr = +frmData.get("hr");
 
-    if (hr<1){
-        return alert ("Please enter a positive number")
-    }
+  if (hr < 1) {
+    return alert("Please enter a positive number");
+  }
 
-    const ttlBadHrs = totalBadHours()
-    const total = taskList.reduce((subttl, item)=> subttl + item.hr, 0) +hr;
-   if (ttlBadHrs+total >hrPerWeak){
-       return alert("You have exceeded the maximum hours per week")
-   }
+  const ttlBadHrs = totalBadHours();
+  const total = taskList.reduce((subttl, item) => subttl + item.hr, 0) + hr;
+  if (ttlBadHrs + total > hrPerWeak) {
+    return alert("You have exceeded the maximum hours per week");
+  }
 
-    const obj = {
-        task,
-        hr,
-    };
-   taskList.push(obj);
+  const obj = {
+    task,
+    hr,
+  };
+  taskList.push(obj);
   display();
-  totalHours()
-   
+  totalHours();
 };
-const display =() =>{
-    // console.log(taskList, "from display");
-    let str = "";
-    taskList.map((item, i) =>{
-        str +=`
+const display = () => {
+  // console.log(taskList, "from display");
+  let str = "";
+  taskList.map(function (item, i) {
+    str += `
         <tr>
                                         <td>
                                             <input type="checkbox" name="" id="">
@@ -65,16 +59,15 @@ const display =() =>{
 
                                         
                                     </tr>
-        `
-    })
-document.getElementById("task-list").innerHTML = str
-    
-}
-const displayBadList =() =>{
-    // console.log(taskList, "from display");
-    let str = "";
-    badList.map((item, i) =>{
-        str +=`
+        `;
+  });
+  document.getElementById("task-list").innerHTML = str;
+};
+const displayBadList = () => {
+  // console.log(taskList, "from display");
+  let str = "";
+  badList.map((item, i) => {
+    str += `
         <tr>
                                         <td>
                                             <input type="checkbox" name="" id="">
@@ -93,64 +86,58 @@ const displayBadList =() =>{
 
                                         
                                     </tr>
-        `
-    })
-document.getElementById("bad-list").innerHTML = str
-    
-}
-
-const deleteBadItem = i =>{
-    if (!confirm("Are you sure you want to delete this item ?")){
-        return;
-    }
-    badList.splice(i, 1);
-    displayBadList();
-    totalHours();
-    totalBadHours();
+        `;
+  });
+  document.getElementById("bad-list").innerHTML = str;
 };
 
-const totalHours = () =>{
-    const total = taskList.reduce((subttl, item)=> subttl + item.hr, 0);
-    const ttlBdHr = totalBadHours()
-    const ttlHrs = total + ttlBdHr
-        
-       document.getElementById("totalHours").innerHTML = ttlHrs;
-    
-    };
-
-const deleteItem = i =>{
-    if (!confirm("Are you sure you want to delete this item ?")){
-        return;
-    }
-    taskList.splice(i, 1)
-    display()
-    totalHours()
-}
-
-const totalBadHours = ()=>{
-
-const total = badList.reduce((subttl, item)=> subttl + item.hr, 0);
-     
-     document.getElementById("totalBadHours").innerText = total;
-return total
+const deleteBadItem = (i) => {
+  if (!confirm("Are you sure you want to delete this item ?")) {
+    return;
+  }
+  badList.splice(i, 1);
+  displayBadList();
+  totalHours();
+  totalBadHours();
 };
 
+const totalHours = () => {
+  const total = taskList.reduce((subttl, item) => subttl + item.hr, 0);
+  const ttlBdHr = totalBadHours();
+  const ttlHrs = total + ttlBdHr;
 
-const markAsNotToDo = i => {
+  document.getElementById("totalHours").innerHTML = ttlHrs;
+};
 
-    const itm = taskList.splice(i, 1);
-    display();
-    badList.push(itm[0]);
-    displayBadList(); 
-    totalBadHours();
-}
-const markAsToDo = i => {
+const deleteItem = (i) => {
+  if (!confirm("Are you sure you want to delete this item ?")) {
+    return;
+  }
+  taskList.splice(i, 1);
+  display();
+  totalHours();
+};
 
-    const itm = badList.splice(i, 1);
-    displayBadList();
+const totalBadHours = () => {
+  const total = badList.reduce((subttl, item) => subttl + item.hr, 0);
 
-    taskList.push(itm[0]);
-    console.log(taskList, i);
-    display(); 
-    totalHours();
-}
+  document.getElementById("totalBadHours").innerText = total;
+  return total;
+};
+
+const markAsNotToDo = (i) => {
+  const itm = taskList.splice(i, 1);
+  display();
+  badList.push(itm[0]);
+  displayBadList();
+  totalBadHours();
+};
+const markAsToDo = (i) => {
+  const itm = badList.splice(i, 1);
+  displayBadList();
+
+  taskList.push(itm[0]);
+  console.log(taskList, i);
+  display();
+  totalHours();
+};
